42867eab550fcbbd9b1932f0c980364cb45bf32b
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-13 21:01:12 +0200

    fix

--- src/controllers/countryInfoController.ts
+++ src/controllers/countryInfoController.ts
@@ -3,21 +3,34 @@ import { Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import {
   getBorders,
-  getFlagUrl,
+  getCountryNameFromList,
+  getFlagData,
   getPopulation,
+  getPopulationByName,
 } from "../services/getCountriesData";
 
 const countryInfoController = catchAsync(
   async (req: Request, res: Response) => {
     const { code } = req.params;
+    let flagUrl;
+    let population;
 
     const borders = await getBorders(code);
 
-    const countryFlag = await getFlagUrl(code);
+    const countryFlagData = await getFlagData(code);
 
-    const population = await getPopulation(countryFlag[0].iso3);
+    if (Object.keys(countryFlagData).length > 0) {
+      flagUrl = countryFlagData[0].flag;
+      population = await getPopulation(countryFlagData[0].iso3);
+    } else {
+      flagUrl = "";
 
-    res.status(200).json({ borders, flagUrl: countryFlag[0].flag, population });
+      let contryName = await getCountryNameFromList(code);
+
+      population = await getPopulationByName(contryName);
+    }
+
+    res.status(200).json({ borders, flagUrl: flagUrl, population });
   }
 );
 
