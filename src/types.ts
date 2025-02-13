3116994b24151deafe8df6350036c59a0cc9a547
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-13 15:32:49 +0200

    add types

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export interface IBorderCountry {
+  commonName: string;
+  officialName: string;
+  countryCode: string;
+  region: string;
+  borders: null;
+}
+
+export interface IPopulation {
+  country: string;
+  code: string;
+  iso3: string;
+  populationCounts: IPopulationCounts[];
+}
+
+export interface IPopulationCounts {
+  year: string;
+  value: string;
+}
+
+export interface IFlag {
+  name: string;
+  flag: string;
+  iso2: string;
+  iso3: string;
+}
